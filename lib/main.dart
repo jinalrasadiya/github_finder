a5637b80f5ec5fd55831d6222520d598fb76be15
jinalrasadiya <jinalrasadiya1234@gmail.com>
2025-08-14 21:45:45 +0530

    Dark-light mode code changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,36 @@ void main() async {
   runApp(const GitHubFinderApp());
 }
 
+class ThemeController extends GetxController {
+  final _storage = GetStorage();
+  final themeMode = ThemeMode.system.obs; // default: follow system
+
+  @override
+  void onInit() {
+    super.onInit();
+    String? savedTheme = _storage.read('themeMode');
+    if (savedTheme != null) {
+      themeMode.value = ThemeMode.values.firstWhere(
+            (mode) => mode.toString() == savedTheme,
+        orElse: () => ThemeMode.system,
+      );
+    }
+  }
+
+  void changeTheme(ThemeMode mode) {
+    themeMode.value = mode;
+    _storage.write('themeMode', mode.toString());
+  }
+}
+
 class GitHubFinderApp extends StatelessWidget {
   const GitHubFinderApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    final themeController = Get.put(ThemeController());
+
+    return Obx(() => GetMaterialApp(
       title: 'GitHub Finder',
       debugShowCheckedModeBanner: false,
       initialBinding: AppBinding(),
@@ -27,7 +51,8 @@ class GitHubFinderApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-    );
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: themeController.themeMode.value,
+    ));
   }
 }
-
